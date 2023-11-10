1325a2eac92549cb06c31825ffb3a2fea3f39e5e
shashikant <shashitnak@gmail.com>
2023-11-11 00:27:21 +0530

    passing 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 use std::{net::{TcpListener, TcpStream}, thread, io::{self, Write, BufRead, BufReader}};
 
+trait HttpStatus: Sized
+where
+    usize: TryFrom<Self>,
+{
+    fn as_msg(self) -> Option<&'static str> {
+        let status_code: usize = self
+            .try_into()
+            .ok()?;
+
+        Some(match status_code {
+            200 => "200 OK",
+            404 => "404 Not Found",
+            _ => None?
+        })
+    }
+}
+
+impl<T> HttpStatus for T
+where
+    usize: TryFrom<T> {}
+
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
     let mut _data = vec![];
     let mut reader = BufReader::new(&stream);
+    let mut status_code = 404;
     loop {
         let mut line = String::new();
         reader.read_line(&mut line)?;
         if line.as_str() == "\r\n" {
             break;
         }
+        if line.starts_with("GET") {
+            if let Some("/") = line
+                .split_ascii_whitespace()
+                .skip(1)
+                .next() {
+                    status_code = 200;
+                }
+        }
         _data.extend_from_slice(line.as_bytes());
     }
-    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    write!(stream, "HTTP/1.1 {}", status_code.as_msg().unwrap())?;
     Ok(())
 }
 
